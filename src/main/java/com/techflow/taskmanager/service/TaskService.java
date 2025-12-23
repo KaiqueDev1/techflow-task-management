ffb3aef71fe9a8641c34827c2b317687e0cfb9d7
KaiqueDev1 <kakamelo141205@gmail.com>
2025-12-23 11:56:32 -0300

    FEAT: Implementação de camadas de serviço para gerenciamento das tarefas

---
+++ src/main/java/com/techflow/taskmanager/service/TaskService.java
@@ -0,0 +1,23 @@
+package com.techflow.taskmanager.service;
+
+import com.techflow.taskmanager.model.Task;
+import com.techflow.taskmanager.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class TaskService {
+    private final TaskRepository repository;
+
+    public TaskService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public Task criar(Task task) {
+        return repository.save(task);
+    }
+
+    public List<Task> listar() {
+        return repository.findAll();
+    }
+}
