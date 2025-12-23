861e11f28286672f15b7308d8cae86b9dec33d5c
KaiqueDev1 <kakamelo141205@gmail.com>
2025-12-23 12:30:30 -0300

    FEAT: Organização de codigo e correção de ponto e virgula

--- src/main/java/com/techflow/taskmanager/model/Task.java
+++ src/main/java/com/techflow/taskmanager/model/Task.java
@@ -6,33 +6,66 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "tasks")
 public class Task {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
     private String titulo;
+
     private String descricao;
 
     @Enumerated(EnumType.STRING)
     private TaskStatus status;
+
     @Enumerated(EnumType.STRING)
     private TaskPriority prioridade;
+
     private LocalDateTime criadoEm;
 
     @PrePersist
     protected void onCreate() {
-        this.criadoEm = LocalDateTime.now()
+        this.criadoEm = LocalDateTime.now();
     }
 
-    public Long getId() {return id;}
-    public String getTitulo() {return titulo;}
-    public void setTitulo(String titulo) {this.titulo = titulo}
-    public String getDescricao() {return descricao;}
-    public void getDescricao(String descricao) {this.descricao = descricao;}
-    public TaskStatus getStatus() {return status;}
-    public void setStatus(TaskStatus status) {this.status = status;}
-    public TaskPriority getPrioridade() { return prioridade; }
-    public void setPrioridade(TaskPriority prioridade) { this.prioridade = prioridade; }
-    public LocalDateTime getCriadoEm() { return criadoEm; }
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public TaskPriority getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(TaskPriority prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
 }
